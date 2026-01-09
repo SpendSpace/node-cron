1482f14fc379b11d5f8cd88241ffc238aa765d30
jlew24asu <jlew24asu@gmail.com>
2026-01-09 06:52:54 -0600

    test

--- src/index.ts
+++ src/index.ts
@@ -31,7 +31,7 @@ async function runBudgetAlerts() {
 }
 
 // Run daily at 12:30 UTC (6:30am CST / 7:30am CDT)
-cron.schedule("48 12 * * *", runBudgetAlerts);
+cron.schedule("56 12 * * *", runBudgetAlerts);
 
 console.log(
   `[${new Date().toISOString()}] Cron service started. Budget alerts scheduled for 12:30 UTC (6:30am CST) daily.`
